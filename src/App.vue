2b04a5abc39b6e37a0cf0c2742883beb1fd90ed6
francescomascellino <franesco.mascellino@gmail.com>
2023-11-17 17:00:10 +0100

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -58,58 +58,6 @@ export default {
 
             <ProjectCard :project="project" :baseUrl="baseUrl" v-for="project in this.projects" />
 
-<!--             <div class="col" v-for="project in projects">
-                <div class="card h-100">
-
-                    <div class="card-header" style="height: 4rem">
-                        <h5>{{ project.title }}</h5>
-                    </div>
-
-                    <img class="img-fluid object-fit-cover" style="height: 300px"
-                        :src="this.baseurl + 'storage/' + project.thumb" :alt="project.title">
-
-                    <div class="card-body">
-                        <p><strong>Description: </strong>{{ project.description }}</p>
-
-                        <p><strong>Type: </strong>
-
-                            <span v-if="project.type"> {{ project.type.name }} </span>
-                            <span v-else>Uncategorized</span>
-                        </p>
-
-                        <p><strong> Technologies used: </strong></p>
-
-                        <div class="d-flex" v-if="project.technologies.length > 0">
-                            <ul class="d-flex g-2 list-unstyled" v-for="technology in project.technologies">
-
-                                <li class="badge bg-success me-1">
-                                    <i class="fa-solid fa-code"></i> {{ technology.name }}
-                                </li>
-
-                            </ul>
-                        </div>
-
-                        <div class="d-flex" v-else>
-                            <ul class="d-flex gap-2 list-unstyled">
-
-                                <li class="badge bg-secondary">
-                                    <i class="fa-regular fa-file"></i> None/Others
-                                </li>
-
-                            </ul>
-                        </div>
-
-                        <p><i class="fa-brands fa-github"></i> <a :href="project.github"
-                                class="text-decoration-none text-black" target="blank">{{ project.github }}</a>
-                        </p>
-
-                        <p><i class="fa-solid fa-link"></i> <a :href="project.link" class="text-decoration-none text-black"
-                                target="blank">{{ project.link }}</a></p>
-
-                    </div>
-
-                </div>
-            </div> -->
         </div>
 
         <nav aria-label="Page navigation">
