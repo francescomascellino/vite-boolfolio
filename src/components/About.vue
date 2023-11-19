d0799a4d361a2eec07386547e31c96a606242c33
francescomascellino <franesco.mascellino@gmail.com>
2023-11-19 17:25:14 +0100

    Add style to About Section

--- src/components/About.vue
+++ src/components/About.vue
@@ -17,11 +17,38 @@ export default {
 
 <template>
     <div class="container h-100">
-        <div class="row align-content-center justify-content-center h-100">
-            <h1 class="text-light text-center">WELCOME</h1>
-            <h2 class="text-light text-center">My name is Francesco and I am a Full Stack Web Developing Student</h2>
+
+        <div class="row h-50 align-items-center grow-left">
+
+            <h1 class="text-light text-center">Hi there 👋</h1>
+            <h2 class="text-light text-center">My name is Francesco and I am a Full Stack Web Developing Student
+            </h2>
+
         </div>
+
+        <div class="row justify-content-end h-50 align-items-center grow-right">
+
+            <div class="col-6 text-light ">
+
+                <h3>💫 About Me:</h3>
+                <ul class="list-unstyled">
+                    <li>🔭 I am currently studing Full-Stack web developement with Boolean Careers<br>📚 I learned JS,
+                        Vue
+                        3, Vite
+                        and SASS</li>
+                    <li>💻 I’m currently learning php, Laravel and other backend techologies</li>
+                    <li>🤝 I love to work with
+                        creative minds!</li>
+                    <li>🎮 Larper, rpg gamer and a bit crazy (●'◡'●)</li>
+                </ul>
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/About.scss';
+</style>
